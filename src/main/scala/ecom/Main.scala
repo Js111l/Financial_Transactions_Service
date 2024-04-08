9c6a3ab8778de66c3b782564b1ac5e793a36bb36
Jakub <jakubswiercz5@gmail.com>
2024-04-08 20:56:33 +0200

    applied fixes

--- src/main/scala/ecom/Main.scala
+++ src/main/scala/ecom/Main.scala
@@ -19,6 +19,6 @@ object Main extends App {
 
 
   val routes = injector.getInstance(classOf[TransactionController]).routes ~ injector.getInstance(classOf[PaymentController]).routes
-  val (host, port) = ("localhost", 9091)
+  val (host, port) = ("0.0.0.0",80 )
   Http().newServerAt(host,port).bind(routes)
 }
